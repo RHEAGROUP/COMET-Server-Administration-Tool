374fb53f8313978a065d6c81f2244226e0e2ab76
Adrian Chivu <adrian.chivu@enea.com>
2020-09-02 16:52:07 +0300

    Show all the engineering models & ref libraries - basic ui.

---
+++ CDP4SAT/Views/Tabs/EngineeringModelLayoutGroup.xaml.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="EngineeringModelLayoutGroup.xaml.cs">
+//    Copyright (c) 2020
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace CDP4SAT.Views.Tabs
+{
+    using DevExpress.Xpf.LayoutControl;
+
+    /// <summary>
+    /// Interaction logic for EngineeringModelLayoutGroup.xaml
+    /// </summary>
+    public partial class EngineeringModelLayoutGroup : LayoutGroup
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EngineeringModelLayoutGroup"/> class.
+        /// </summary>
+        public EngineeringModelLayoutGroup()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CDP4SAT/Views/Tabs/SiteRdlLayoutGroup.xaml.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SiteRdlLayoutGroup.xaml.cs">
+//    Copyright (c) 2020
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace CDP4SAT.Views.Tabs
+{
+    using DevExpress.Xpf.LayoutControl;
+
+    /// <summary>
+    /// Interaction logic for SiteRdlLayoutGroup.xaml
+    /// </summary>
+    public partial class SiteRdlLayoutGroup : LayoutGroup
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SiteRdlLayoutGroup"/> class.
+        /// </summary>
+        public SiteRdlLayoutGroup()
+        {
+            InitializeComponent();
+        }
+    }
+}
