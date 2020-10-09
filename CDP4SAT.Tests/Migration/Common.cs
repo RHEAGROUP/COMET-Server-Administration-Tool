352dfb54060bb680089c650e5ae0545a3844234f
Adrian Chivu <adrian.chivu@enea.com>
2020-10-09 15:20:39 +0300

    Added a basic test suite. There is room for more unit tests.

---
+++ CDP4SAT.Tests/Migration/Common.cs
@@ -0,0 +1,21 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CommonTest.cs">
+//    Copyright (c) 2020
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace App.Tests.Migration
+{
+    /// <summary>
+    /// Common class that will be used by all suite tests
+    /// </summary>
+    public abstract class CommonTest
+    {
+        protected const string SourceServerUri = "https://cdp4services-public.cdp4.org";
+        protected const string SourceUsername = "admin";
+        protected const string SourcePassword = "pass";
+        protected const string TargetServerUri = "http://localhost:5000";
+        protected const string TargetUsername = "admin";
+        protected const string TargetPassword = "pass";
+    }
+}
---
+++ CDP4SAT.Tests/Migration/MigrationTestFixture.cs
@@ -0,0 +1,91 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MigrationTestFixture.cs">
+//    Copyright (c) 2020
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace App.Tests.Migration
+{
+    using System.Reactive.Concurrency;
+    using ReactiveUI;
+    using Moq;
+    using NUnit.Framework;
+    using System.Collections.Generic;
+    using global::Migration.ViewModels;
+    using global::Migration.ViewModels.Common;
+
+    /// <summary>
+    /// Suite of tests for the <see cref="Migration"/> <see cref="MigrationViewModel"/>
+    /// </summary>
+    [TestFixture]
+    public class MigrationTestFixture : CommonTest
+    {
+        private Mock<MigrationViewModel> migrationViewModel;
+
+        /// <summary>
+        /// Set target and source servers required by the migration
+        /// </summary>
+        private void SetTargetAndSourceServers()
+        {
+            // Migration will always fail when the target server does not contains models and in order to 'pass' the tests the target server it's the same with the source server
+
+            this.migrationViewModel.Object.SourceViewModel = new LoginViewModel
+            {
+                ServerType = new KeyValuePair<string, string>("CDP", "CDP4 WebServices"),
+                UserName = SourceUsername,
+                Password = SourcePassword,
+                Uri = SourceServerUri
+            };
+
+            this.migrationViewModel.Object.TargetViewModel = new LoginViewModel
+            {
+                ServerType = new KeyValuePair<string, string>("CDP", "CDP4 WebServices"),
+                UserName = SourceUsername, // TargetUsername
+                Password = SourcePassword, // TargetPassword
+                Uri = SourceServerUri      // TargetServerUri
+            };
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            RxApp.MainThreadScheduler = Scheduler.CurrentThread;
+
+            this.migrationViewModel = new Mock<MigrationViewModel>();
+            this.migrationViewModel.Object.AddSubscriptions();
+        }
+
+        [Test]
+        public void VerifyIfMigrationStartWithSourceAndTargetSessionSet()
+        {
+            SetTargetAndSourceServers();
+
+            Assert.DoesNotThrowAsync(async () =>
+                await this.migrationViewModel.Object.SourceViewModel.LoginCommand.ExecuteAsyncTask());
+            Assert.AreEqual(true, this.migrationViewModel.Object.SourceViewModel.LoginSuccessfully);
+
+            Assert.DoesNotThrowAsync(async () =>
+                await this.migrationViewModel.Object.TargetViewModel.LoginCommand.ExecuteAsyncTask());
+            Assert.AreEqual(true, this.migrationViewModel.Object.TargetViewModel.LoginSuccessfully);
+
+            Assert.IsTrue(this.migrationViewModel.Object.CanMigrate);
+
+            Assert.DoesNotThrowAsync(async () =>
+                await this.migrationViewModel.Object.MigrateCommand.ExecuteAsyncTask());
+
+            Assert.DoesNotThrowAsync(async () =>
+                await this.migrationViewModel.Object.SourceViewModel.ServerSession.Close());
+            Assert.DoesNotThrowAsync(async () =>
+                await this.migrationViewModel.Object.TargetViewModel.ServerSession.Close());
+        }
+
+        [Test]
+        public void VerifyIfMigrationNotStartWithoutSourceAndTargetSessionSet()
+        {
+            SetTargetAndSourceServers();
+
+            Assert.IsNull(this.migrationViewModel.Object.SourceViewModel.ServerSession);
+            Assert.IsNull(this.migrationViewModel.Object.TargetViewModel.ServerSession);
+        }
+    }
+}
