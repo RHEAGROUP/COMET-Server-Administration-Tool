bde848a83bd6af435e386c2472689c2a889392db
Cozmin Velciu <cozmin.velciu@enea.com>
2021-04-27 22:19:09 +0300

    Format LogEvents

--- Modules/Migration/ViewModels/FixCardinalityErrorsDialogViewModel.cs
+++ Modules/Migration/ViewModels/FixCardinalityErrorsDialogViewModel.cs
@@ -135,10 +135,7 @@ namespace Migration.ViewModels
         {
             if (this.migrationSourceSession is null)
             {
-                CDPMessageBus.Current.SendMessage(new LogEvent
-                {
-                    Message = "The source session is not defined"
-                });
+                CDPMessageBus.Current.SendMessage(new LogEvent { Message = "The source session is not defined" });
                 return;
             }
 
@@ -158,13 +155,11 @@ namespace Migration.ViewModels
         {
             var result = new List<PocoErrorRowViewModel>();
 
-            foreach (var thing in this.migrationSourceSession.Assembler.Cache.Select(item => item.Value.Value)
+            foreach (var thing in this.migrationSourceSession.Assembler.Cache
+                .Select(item => item.Value.Value)
                 .Where(t => t.ValidationErrors.Any()))
             {
-                foreach (var error in thing.ValidationErrors)
-                {
-                    result.Add(new PocoErrorRowViewModel(thing, error));
-                }
+                result.AddRange(thing.ValidationErrors.Select(error => new PocoErrorRowViewModel(thing, error)));
             }
 
             return result;
@@ -177,7 +172,7 @@ namespace Migration.ViewModels
         {
             this.IsBusy = true;
 
-            CDPMessageBus.Current.SendMessage(new LogEvent { Message = "Fix the cardinality errors list for the selected models" });
+            CDPMessageBus.Current.SendMessage(new LogEvent { Message = "Fixing the cardinality errors for the selected models..." });
 
             foreach (var rowError in this.Errors)
             {
@@ -195,8 +190,8 @@ namespace Migration.ViewModels
             this.Errors.AddRange(d);
 
             CDPMessageBus.Current.SendMessage(this.Errors.Count == 0
-                ? new LogEvent {Message = "The cardinality errors list has been successfully fixed"}
-                : new LogEvent {Message = "The cardinality errors list has not been fixed"});
+                ? new LogEvent { Message = "The cardinality errors have been successfully fixed" }
+                : new LogEvent { Message = "The cardinality errors have not been fixed" });
 
             this.IsBusy = false;
         }
