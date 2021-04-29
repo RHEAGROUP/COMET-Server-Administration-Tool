32485e91116e374f29b8bd4e7227e009ca935721
Adrian Chivu <adrian.chivu@enea.com>
2021-04-29 15:31:49 +0300

    Increase test coverage.

--- Modules/Migration.Tests/LoginViewModelTestFixture.cs
+++ Modules/Migration.Tests/LoginViewModelTestFixture.cs
@@ -1,23 +1,23 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="LoginViewModelTestFixture.cs" company="RHEA System S.A.">
 //    Copyright (c) 2015-2021 RHEA System S.A.
-// 
+//
 //    Author: Adrian Chivu, Cozmin Velciu, Alex Vorobiev
-// 
+//
 //    This file is part of CDP4-Server-Administration-Tool.
 //    The CDP4-Server-Administration-Tool is an ECSS-E-TM-10-25 Compliant tool
 //    for advanced server administration.
-// 
+//
 //    The CDP4-Server-Administration-Tool is free software; you can redistribute it and/or modify
 //    it under the terms of the GNU Affero General Public License as
 //    published by the Free Software Foundation; either version 3 of the
 //    License, or (at your option) any later version.
-// 
+//
 //    The CDP4-Server-Administration-Tool is distributed in the hope that it will be useful,
 //    but WITHOUT ANY WARRANTY; without even the implied warranty of
 //    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 //    Affero General Public License for more details.
-// 
+//
 //    You should have received a copy of the GNU Affero General Public License
 //    along with this program. If not, see <http://www.gnu.org/licenses/>.
 // </copyright>
@@ -35,6 +35,7 @@ namespace Migration.Tests
     using CDP4Dal.DAL;
     using Common.Settings;
     using Common.ViewModels;
+    using Common.ViewModels.PlainObjects;
     using Moq;
     using NUnit.Framework;
     using ReactiveUI;
@@ -94,6 +95,7 @@ namespace Migration.Tests
             };
 
             this.loginViewModel.Object.Dal = mockDal.Object;
+
             mockDal.Setup(x => x.Open(It.IsAny<Credentials>(), It.IsAny<CancellationToken>())).Returns<Credentials, CancellationToken>((credentials, cancellationToken) =>
             {
                 var result = this.dictionaryThings.Values.ToList() as IEnumerable<Thing>;
@@ -105,18 +107,32 @@ namespace Migration.Tests
         public void VerifyGetterSetters()
         {
             Assert.AreEqual(DataSource.CDP4, this.loginViewModel.Object.SelectedDataSource);
+            this.loginViewModel.Object.SelectedDataSource = DataSource.WSP;
+            Assert.AreEqual(DataSource.WSP, this.loginViewModel.Object.SelectedDataSource);
+
+            this.loginViewModel.Object.UserName = this.person.ShortName;
             Assert.AreEqual(this.person.ShortName, this.loginViewModel.Object.UserName);
+
+            this.loginViewModel.Object.Password = this.person.Password;
             Assert.AreEqual(this.person.Password, this.loginViewModel.Object.Password);
+
+            this.loginViewModel.Object.Uri = ServerUrl;
             Assert.AreEqual(ServerUrl, this.loginViewModel.Object.Uri);
+
+            Assert.AreEqual(false, this.loginViewModel.Object.JsonIsSelected);
+            Assert.IsNull(this.loginViewModel.Object.Output);
+
+            this.loginViewModel.Object.EngineeringModels = new List<EngineeringModelRowViewModel>();
+            Assert.IsEmpty(this.loginViewModel.Object.EngineeringModels);
         }
 
         [Test]
         public void VerifyIfLoginSucceeded()
         {
             Assert.DoesNotThrowAsync(async () => await this.loginViewModel.Object.LoginCommand.ExecuteAsyncTask());
-            
+
             Assert.AreEqual(true, this.loginViewModel.Object.LoginSuccessfully);
-            
+
             Assert.DoesNotThrowAsync(async () => await this.loginViewModel.Object.ServerSession.Close());
         }
 
