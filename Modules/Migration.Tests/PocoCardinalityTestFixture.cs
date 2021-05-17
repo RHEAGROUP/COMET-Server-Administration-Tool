57f88c33211fc99466aa5bb5ed376298bd8a5ce2
Adrian Chivu <adrian.chivu@enea.com>
2021-05-17 19:29:21 +0300

    Increase code coverage.

--- Modules/Migration.Tests/PocoCardinalityTestFixture.cs
+++ Modules/Migration.Tests/PocoCardinalityTestFixture.cs
@@ -4,6 +4,8 @@
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
+using System.Collections.Generic;
+
 namespace Migration.Tests
 {
     using System;
@@ -383,6 +385,84 @@ namespace Migration.Tests
 
             Assert.That(this.viewModel.SelectedError != null);
             Assert.That(this.viewModel.ErrorDetails != null);
+
+            Assert.DoesNotThrowAsync(async () => await this.session.Object.Close());
+        }
+
+        [Test]
+        public void VerifyThatValidatePocoPropertiesAddsScalarParameterTypeError()
+        {
+            Assert.DoesNotThrowAsync(async () => await this.session.Object.Open());
+
+            //var elementDefinition = new ElementDefinition(Guid.NewGuid(), this.session.Object.Assembler.Cache,
+            //    this.session.Object.Credentials.Uri);
+            //var definition = new Definition(Guid.NewGuid(), this.session.Object.Assembler.Cache,
+            //    this.session.Object.Credentials.Uri);
+            var scalarParameterType = new SimpleQuantityKind(Guid.NewGuid(), this.session.Object.Assembler.Cache,
+                this.session.Object.Credentials.Uri) as ScalarParameterType;
+            this.siteReferenceDataLibrary.ParameterType.Add(scalarParameterType);
+            //var scalarParameter = new Parameter(Guid.NewGuid(), this.session.Object.Assembler.Cache,
+            //    this.session.Object.Credentials.Uri) { Owner = this.domain };
+            //scalarParameter.ParameterType = scalarParameterType;
+            //elementDefinition.Parameter.Add(scalarParameter);
+
+            //this.iteration.Element.Add(elementDefinition);
+            //elementDefinition.Definition.Add(definition);
+
+            scalarParameterType.ValidatePoco();
+
+            this.session.Object.Assembler.Cache.TryAdd(new CacheKey(scalarParameterType.Iid, null),
+                new Lazy<Thing>(() => scalarParameterType));
+
+            this.viewModel.BindPocoErrors();
+
+            Assert.That(this.viewModel.Errors.Any(e => e.Error.Contains("The property Symbol is null")));
+
+            Assert.DoesNotThrow(() => this.viewModel.FixCommand.Execute(null));
+
+            Assert.DoesNotThrowAsync(async () => await this.session.Object.Close());
+        }
+
+        [Test]
+        public void VerifyThatValidatePocoPropertiesFixValueSets()
+        {
+            Assert.DoesNotThrowAsync(async () => await this.session.Object.Open());
+
+            var elementDefinition = new ElementDefinition(Guid.NewGuid(), this.session.Object.Assembler.Cache,
+                this.session.Object.Credentials.Uri);
+            var definition = new Definition(Guid.NewGuid(), this.session.Object.Assembler.Cache,
+                this.session.Object.Credentials.Uri);
+            var parameter = new Parameter(Guid.NewGuid(), this.session.Object.Assembler.Cache,
+                this.session.Object.Credentials.Uri){ Owner = this.domain };
+            var valueset = new ParameterValueSet(Guid.NewGuid(), this.session.Object.Assembler.Cache,
+                this.session.Object.Credentials.Uri);
+
+            parameter.ValueSet.Add(valueset);
+
+            elementDefinition.Parameter.Add(parameter);
+
+            this.iteration.Element.Add(elementDefinition);
+            elementDefinition.Definition.Add(definition);
+
+            valueset.ValidatePoco();
+            parameter.ValidatePoco();
+
+            this.session.Object.Assembler.Cache.TryAdd(new CacheKey(parameter.Iid, null),
+                new Lazy<Thing>(() => parameter));
+            this.session.Object.Assembler.Cache.TryAdd(new CacheKey(valueset.Iid, null),
+                new Lazy<Thing>(() => valueset));
+
+            this.viewModel.BindPocoErrors();
+
+            Assert.That(this.viewModel.Errors.Any(e => e.ContainerThingClassKind == ClassKind.ParameterValueSet.ToString() && e.Error.Contains("The number of elements in the property Formula is wrong. It should be at least 1")));
+
+            Assert.That(this.viewModel.Errors.Any(e => e.ContainerThingClassKind == ClassKind.ParameterValueSet.ToString() && e.Error.Contains("The number of elements in the property Manual is wrong. It should be at least 1.")));
+
+            Assert.That(this.viewModel.Errors.Any(e => e.ContainerThingClassKind == ClassKind.ParameterValueSet.ToString() && e.Error.Contains("The number of elements in the property Published is wrong. It should be at least 1.")));
+
+            Assert.That(this.viewModel.Errors.Any(e => e.ContainerThingClassKind == ClassKind.ParameterValueSet.ToString() && e.Error.Contains("The number of elements in the property Reference is wrong. It should be at least 1.")));
+
+            Assert.DoesNotThrow(() => this.viewModel.FixCommand.Execute(null));
 
             Assert.DoesNotThrowAsync(async () => await this.session.Object.Close());
         }
