425e2463644c9e0775cbb013ec4ac097aec1ad75
Adrian Chivu <adch1@enea.com>
2021-06-24 15:29:41 +0300

    Fix some constants values.

--- Modules/StressGenerator/ViewModels/StressGeneratorConfiguration.cs
+++ Modules/StressGenerator/ViewModels/StressGeneratorConfiguration.cs
@@ -26,6 +26,7 @@
 namespace StressGenerator.ViewModels
 {
     using System.Collections.Generic;
+    using CDP4Common.SiteDirectoryData;
     using CDP4Dal;
 
     /// <summary>
@@ -36,12 +37,22 @@ namespace StressGenerator.ViewModels
         /// <summary>
         /// Stress generator models prefix
         /// </summary>
-        public const string ModelPrefix = "StressTester";
+        public const string ModelPrefix = "StressTester_TemporaryTestModel";
 
         /// <summary>
         /// Generic RDL short name
         /// </summary>
         public const string GenericRdlShortName = "Generic_RDL";
+
+        /// <summary>
+        /// Generic ElementDefinition name
+        /// </summary>
+        public const string GenericElementName = "Element";
+
+        /// <summary>
+        /// Generic ElementDefinition short name
+        /// </summary>
+        public const string GenericElementShortName = "ED";
 
         /// <summary>
         /// Minimum time interval in seconds for test data generation
@@ -73,32 +84,52 @@ namespace StressGenerator.ViewModels
         /// <summary>
         /// Gets or sets the time interval in seconds for test data generation
         /// </summary>
-        public int TimeInterval { get; private set; }
+        public int TimeInterval { get; set; }
+
+        /// <summary>
+        /// Back-tier field of the <see cref="TestObjectsNumber"/>
+        /// </summary>
+        private int testObjectsNumber;
 
         /// <summary>
         /// Gets or sets the number of the test objects to be generated
         /// </summary>
-        public int TestObjectsNumber { get; private set; }
+        public int TestObjectsNumber
+        {
+            get => this.testObjectsNumber;
+            set
+            {
+                if (value < MinNumberOfTestObjects)
+                {
+                    this.testObjectsNumber = MinNumberOfTestObjects;
+                }
+
+                if (value > MaxNumberOfTestObjects)
+                {
+                    this.testObjectsNumber = MaxNumberOfTestObjects;
+                }
+            }
+        }
 
         /// <summary>
         /// Gets or sets the first part of generated element definition name.
         /// </summary>
-        public string ElementName { get; private set; }
+        public string ElementName { get; set; }
 
         /// <summary>
         /// Gets or sets the first part of generated element definition short name.
         /// </summary>
-        public string ElementShortName { get; private set; }
+        public string ElementShortName { get; set; }
 
         /// <summary>
         /// Gets or sets a flag that trigger deleting of all elements in the engineering model.
         /// </summary>
-        public bool DeleteAllElements { get; private set; }
+        public bool DeleteAllElements { get; set; }
 
         /// <summary>
         /// Gets or sets a flag that trigger deleting of the engineering model.
         /// </summary>
-        public bool DeleteModel { get; private set; }
+        public bool DeleteModel { get; set; }
 
         /// <summary>
         /// Currently open server session
@@ -106,36 +137,36 @@ namespace StressGenerator.ViewModels
         public ISession Session { get; private set; }
 
         /// <summary>
+        /// EngineeringModelSetup used for stress test <see cref="EngineeringModelSetup"/>
+        /// </summary>
+        public EngineeringModelSetup TestModelSetup { get; set; }
+
+        /// <summary>
+        /// Source EngineeringModelSetup for the TestModelSetup
+        /// </summary>
+        public EngineeringModelSetup SourceModelSetup { get; set; }
+
+        /// <summary>
+        /// EngineeringModelSetup name used for stress test
+        /// </summary>
+        public string TestModelSetupName { get; set; }
+
+        /// <summary>
+        /// Supported mode <see cref="SupportedOperationModes"/>
+        /// </summary>
+        public SupportedOperationModes OperationMode { get; set; }
+
+        /// <summary>
         /// Initialize a new instance of <see cref="StressGeneratorConfiguration" />
         /// </summary>
         /// <param name="session">Server session <see cref="ISession"/></param>
-        /// <param name="timeInterval">Time interval in seconds for test data generation</param>
-        /// <param name="testObjectsNumber">The number of the test objects to be generated</param>
-        /// <param name="elementName">First part of generated element definition name</param>
-        /// <param name="elementShortName">First part of generated element definition short name</param>
-        /// <param name="deleteAllElements">Flag that trigger deleting of all elements in the engineering model</param>
-        /// <param name="deleteModel"></param>
-        public StressGeneratorConfiguration(ISession session, int timeInterval, int testObjectsNumber,
-            string elementName, string elementShortName, bool deleteAllElements, bool deleteModel)
+        public StressGeneratorConfiguration(ISession session)
         {
             this.Session = session;
-            this.TimeInterval = timeInterval * 1000;
-            this.TestObjectsNumber = testObjectsNumber;
+            this.TestObjectsNumber = 5;
 
-            if (this.TestObjectsNumber < MinNumberOfTestObjects)
-            {
-                this.TestObjectsNumber = MinNumberOfTestObjects;
-            }
-
-            if (this.TestObjectsNumber > MaxNumberOfTestObjects)
-            {
-                this.TestObjectsNumber = MaxNumberOfTestObjects;
-            }
-
-            this.ElementName = elementName.Trim();
-            this.ElementShortName = elementShortName.Trim().Replace(" ", "_");
-            this.DeleteAllElements = deleteAllElements;
-            this.DeleteModel = deleteModel;
+            this.ElementName = GenericElementName;
+            this.ElementShortName = GenericElementShortName;
         }
     }
 }
